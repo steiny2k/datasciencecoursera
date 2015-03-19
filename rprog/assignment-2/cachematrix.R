c6aeed6ed847b0564dcdd51f0a8136cf0d50847d
Sebastian Steinmetz <me@sebastiansteinmetz.ch>
2015-03-19 08:46:14 +0100

    Assignment 2, peer reviewed

---
+++ rprog/assignment-2/cachematrix.R
@@ -0,0 +1,32 @@
+## In order to save computing cycles especially in 
+
+## make a special "matrix" object which can cache its inverse.
+## methods getInverse() and setInverse() are used for setting and
+## getting the cached values
+makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(inverse) m <<- inverse
+  getInverse <- function() m
+  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
+}
+
+
+## Write a short comment describing this function
+
+cacheSolve <- function(x, ...) {
+        ## Return a matrix that is the inverse of 'x'
+  m <- x$getInverse()
+  if (!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setInverse(m)
+  m
+}
