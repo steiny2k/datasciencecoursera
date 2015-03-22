eb19c2680ba84fe9e8784cabf650afc346034c3f
Sebastian Steinmetz <me@sebastiansteinmetz.ch>
2015-03-22 21:20:26 +0000

    comments for assignment-2

--- rprog/assignment-2/cachematrix.R
+++ rprog/assignment-2/cachematrix.R
@@ -1,8 +1,11 @@
-## In order to save computing cycles especially in 
+## In order to save computing cycles especially in repeated calculations, 
+## we can use lexical scoping rules to cache the results of a calculation
+## in a local field of an object.
 
 ## make a special "matrix" object which can cache its inverse.
 ## methods getInverse() and setInverse() are used for setting and
-## getting the cached values
+## getting the cached values. Methods get() and set() are used to
+## store the matrix itself.
 makeCacheMatrix <- function(x = matrix()) {
   m <- NULL
   set <- function(y) {
@@ -16,10 +19,13 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
-
+## this method tries to first pull the cached value from a 
+## makeCacheMatrix-object. In case it exists, no further calculations
+## are done, but the cached value is returned. In case, the value
+## has not yet been calculated, we calculate it, and store it in
+## the cache of the makeCacheMatrix object.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        ## Return a matrix that is the inverse of 'x', if exists.
   m <- x$getInverse()
   if (!is.null(m)) {
     message("getting cached data")
