152d6d7832eb42a0b520c3e83c4e894b702de249
Sebastian Steinmetz <me@sebastiansteinmetz.ch>
2015-05-12 08:56:59 +0200

    Reproducible research quizz 01

---
+++ 05 - repdata/quizz - 01/quizz - 01.R
@@ -0,0 +1,50 @@
+# Q1: Suppose I conduct a study and publish my findings. Which of the following 
+#     is an example of a replication of my study?
+
+# A1: An investigator at another institution conducts a study addressing the 
+#     same question, collects her own data, analyzes it separately from me, and 
+#     publishes her own findings.
+
+# Q2: Which of the following is a requirement for a published data analysis to 
+#     be reproducible?
+
+# A2: The investigator makes the analytic data publicly available.
+
+# Q3: Which of the following is an example of a reproducible study?
+
+# A3: The study's analytic data and computer code for the data analysis are 
+#     publicly available. When the code is run on the analytic data, the 
+#     findings are identical to the published results.
+
+# Q4: Which of the following is a reason that a study might NOT be fully 
+#     replicated?
+
+# A4: The original study was opportunistic in its timing and it would be 
+#     difficult to find a similar context in which to repeat it.
+
+# Q5: Which of the following is a reason why publishing reproducible research is
+#     increasingly important?
+
+# A5: New technologies are increasing the rate of data collection, creating 
+#     datasets that are more complex and extremely high dimensional.
+
+# Q6: What is the role of processing code in the research pipeline?
+
+# A6: It transforms the measured data into analytic data.
+
+# Q7: Which is a goal of literate statistical programming?
+
+# A7: Combine explanatory text and data analysis code in a single document.
+
+# Q8: What does it mean to weave a literate statistical program?
+
+# A8: Transform the literate program into a human readable document.
+
+# Q9: Which of the following is required to implement a literate programming 
+#     system?
+
+# A9: A programming language like R.
+
+# Q10: What is one way in which the knitr system differs from Sweave?
+
+# A10: knitr allows for the use of markdown instead of LaTeX.
